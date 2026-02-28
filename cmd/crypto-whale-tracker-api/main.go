9d5726c85a07ed9282defee3080f67a9b33e2e6b
Alexey <tokyogodb@gmail.com>
2026-02-28 22:08:55 +0200

    feat: add connectWebsocket func

--- cmd/crypto-whale-tracker-api/main.go
+++ cmd/crypto-whale-tracker-api/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/ukique/crypto-whale-tracker-api/internal/features/whale/transport/exchange"
 )
 
 func main() {
+	exchange.ConnectWebSocket()
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal("Fail to start on port 8081:", err)
 	}
---
+++ internal/features/whale/transport/exchange/websocket.go
@@ -0,0 +1,102 @@
+package exchange
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+
+	pb "github.com/ukique/crypto-whale-tracker-api/internal/pb"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	WebsocketURL = "wss://wbs-api.mexc.com/ws"
+)
+
+func ConnectWebSocket() {
+	//graceful shutdown when we use Ctrl + C
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	conn, _, err := websocket.DefaultDialer.Dial(WebsocketURL, nil)
+	if err != nil {
+		log.Fatal("fail dial connection:", err)
+	}
+
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println("fail close connection", err)
+		}
+	}()
+
+	done := make(chan struct{})
+
+	subscriptionMessage := map[string]interface{}{
+		"method": "SUBSCRIPTION",
+		"params": []string{"spot@public.aggre.deals.v3.api.pb@100ms@BTCUSDT"},
+	}
+	subMsg, _ := json.Marshal(subscriptionMessage)
+
+	//if there is no valid subscription on the websocket,
+	//the MEXC server will actively disconnect after 30 second
+	//so we need send PING
+	pingMessage := map[string]string{
+		"method": "PING",
+	}
+	pingMsg, _ := json.Marshal(pingMessage)
+
+	if err = conn.WriteMessage(websocket.TextMessage, subMsg); err != nil {
+		log.Println("fail to write subscription message:", err)
+		return
+	}
+	log.Printf("Send subscription message: %s", subMsg)
+
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		defer close(done)
+		for {
+			select {
+			case <-ticker.C:
+				if err := conn.WriteMessage(websocket.TextMessage, pingMsg); err != nil {
+					log.Println("fail write PING message:", err)
+					return
+				}
+				log.Println("send PING message:", pingMsg)
+
+			case <-interrupt:
+				err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+				if err != nil {
+					log.Println("fail write close message:", err)
+					return
+				}
+				return
+			}
+		}
+	}()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("fail to read message:", err)
+			return
+		}
+		if len(message) > 0 && message[0] == '{' {
+			log.Printf("JSON message (skipping): %s", message)
+			continue
+		}
+
+		decodedMessage := &pb.PushDataV3ApiWrapper{}
+		if err := proto.Unmarshal(message, decodedMessage); err != nil {
+			log.Println("protobuf unmarshal error:", err)
+			continue
+		}
+		log.Println("message:", decodedMessage)
+		//	than here will be check whale func
+	}
+}
