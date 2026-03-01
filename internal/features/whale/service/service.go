e7fca561c081bc67c1ea1f04d718c4f6b077fd40
Alexey <tokyogodb@gmail.com>
2026-03-01 12:04:52 +0200

    feat: add IsWhale func

--- internal/features/whale/service/service.go
+++ internal/features/whale/service/service.go
@@ -1,3 +1,23 @@
 package service
 
-// whale check func
+import (
+	"log"
+	"strconv"
+)
+
+func IsWhale(price string, quantity string) bool {
+	priceFloat, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		log.Println("fail to convert price:", err)
+		return false
+	}
+	quantityFloat, err := strconv.ParseFloat(quantity, 64)
+	if err != nil {
+		log.Println("fail to convert quantity:", err)
+		return false
+	}
+	if priceFloat*quantityFloat >= 50000 {
+		return true
+	}
+	return false
+}
